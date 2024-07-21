2755bec47e0c6af6a3464dd038ed9bff004af820
Minor Gordon <minorg@server.fake>
2024-07-21 10:40:27 -0400

    NodeShape.closed

--- NodeShape.ts
+++ NodeShape.ts
@@ -3,8 +3,13 @@ import { PropertyShape } from "./PropertyShape.js";
 import { BlankNode, NamedNode } from "@rdfjs/types";
 import { sh } from "@tpluscode/rdf-ns-builders";
 import { Maybe } from "purify-ts";
+import { mapTermToBoolean } from "./mapTermToBoolean.js";
 
 export class NodeShape extends Shape {
+  get closed(): Maybe<boolean> {
+    return this.findAndMapObject(sh.closed, mapTermToBoolean);
+  }
+
   get properties(): readonly PropertyShape[] {
     return this.filterAndMapObjects(sh.property, (propertyObject) => {
       switch (propertyObject.termType) {
--- __tests__/NodeShape.test.ts
+++ __tests__/NodeShape.test.ts
@@ -1,27 +1,32 @@
-import { ShapesGraph } from "..";
+import { NodeShape, ShapesGraph } from "..";
 import { DataFactory } from "n3";
-import { beforeAll, describe, it } from "vitest";
+import { beforeAll, describe, expect, it } from "vitest";
 import { testData } from "./testData";
 
 describe("NodeShape", () => {
-  let shapes: ShapesGraph;
+  let addressNodeShape: NodeShape;
+  let personNodeShape: NodeShape;
 
   beforeAll(() => {
-    shapes = ShapesGraph.fromDataset(testData.shapesGraph);
+    const shapesGraph = ShapesGraph.fromDataset(testData.shapesGraph);
+
+    addressNodeShape = shapesGraph
+      .nodeShapeByNode(DataFactory.namedNode("http://schema.org/AddressShape"))
+      .unsafeCoerce();
+    expect(addressNodeShape).toBeDefined();
+
+    personNodeShape = shapesGraph
+      .nodeShapeByNode(DataFactory.namedNode("http://schema.org/PersonShape"))
+      .unsafeCoerce();
+    expect(personNodeShape).toBeDefined();
   });
 
   it("should have properties", ({ expect }) => {
-    {
-      const personNodeShape = shapes.nodeShapeByNode(
-        DataFactory.namedNode("http://schema.org/PersonShape"),
-      );
-      expect(personNodeShape.properties).toHaveLength(4);
-    }
-    {
-      const addressNodeShape = shapes.nodeShapeByNode(
-        DataFactory.namedNode("http://schema.org/AddressShape"),
-      );
-      expect(addressNodeShape.properties).toHaveLength(2);
-    }
+    expect(personNodeShape.properties).toHaveLength(4);
+    expect(addressNodeShape.properties).toHaveLength(2);
+  });
+
+  it("should be closed", ({ expect }) => {
+    expect(addressNodeShape.closed.unsafeCoerce()).toStrictEqual(true);
   });
 });
