68da2a47c7fb0c32d4e17cab2aed640884733ac9
Minor Gordon <minorg@server.fake>
2024-08-24 19:43:34 -0400

    enable coverage reporting

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,12 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   test: {
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "json-summary", "json"],
+      // If you want a coverage reports even if your tests are failing, include the reportOnFailure option
+      reportOnFailure: true,
+    },
     include: ["__tests__/*.test.ts"],
   },
 });
